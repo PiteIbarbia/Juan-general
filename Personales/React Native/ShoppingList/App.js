15c9af35af4dbcbfa57b0253a6b2772a66da7965
Your Name <juanignacioibarbia@gmail.com>
2021-02-25 11:39:10 -0300

    test

---
+++ Personales/React Native/ShoppingList/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+
+const App = () => {
+  return (
+    <View styel={styles.container} >
+      <Text>Hello World</Text>
+    </View>
+  )
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+
+});
+
+export default App;
