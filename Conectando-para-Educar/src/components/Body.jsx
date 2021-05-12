25235b3c28af58955e35107c7f2c21db64093afa
PiteIbarbia <juanignacioibarbia@gmail.com>
2021-05-12 17:13:34 -0300

    gh-pages agregado

--- Conectando-para-Educar/src/components/Body.jsx
+++ Conectando-para-Educar/src/components/Body.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Card, Carousel } from 'react-bootstrap';
-import img0 from '../img-jpg/prueba-0.jpg';
-import img1 from '../img-jpg/prueba-2.jpg';
-import img2 from '../img-jpg/prueba-1.jpg';
-import img3 from '../img-jpg/prueba-3.jpg';
 
 export default function Body(props) {
     return(
